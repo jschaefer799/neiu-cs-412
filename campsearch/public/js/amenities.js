2b83af17b18bff93eb1f8c4920c9955d24a0ba67
jschaefer799 <70353044+jschaefer799@users.noreply.github.com>
2020-10-12 00:43:49 -0500

    Add existing file

--- campsearch/public/js/amenities.js
+++ campsearch/public/js/amenities.js
@@ -1,7 +1,13 @@
 let campsites = require('./campsites')
+let EventEmitter = require('events').EventEmitter
+
+let emitter = new EventEmitter()
 
 exports.filterByDogsAllowed = function (){
-    let sites = campsites.getCampsites()
+    emitter.on('foo', function (arg1){
+        console.log("Emitter received event number: " + arg1)
+    })
+    let sites = campsites.getCampsites(emitter)
 
     console.log("3 of the " + sites + " allow dogs.")
 }
--- campsearch/public/js/campsites.js
+++ campsearch/public/js/campsites.js
@@ -1,8 +1,12 @@
+let DateTime = require('luxon').DateTime
+//alternatively I could use: let luxon = require('luxon) in order to access all of the luxon properties; whereas placing the
+// .DateTime after the statement gives me access to only the DateTime functions
 let reviews = require('./reviews')
 
-exports.getCampsites = function (){
+exports.getCampsites = function (emitter){
     let stars = reviews.getReviews();
     let num = 0;
+    let current = DateTime.local()
 
     if (stars === 1 || stars === 2)
         num = 6
@@ -10,6 +14,15 @@ exports.getCampsites = function (){
         num = 15
     if (stars === 5)
         num = 18
-    console.log("Here's the number of campsites with " + stars + " star reviews in the area you selected:" + num)
+    console.log("As of " + current.toLocaleString() + " the number of campsites with " + stars + " star reviews in the area you selected:" + num)
+
+    let count = 1
+
+    while (count <= 10){
+        console.log("Pre-emitter message for count: " + count)
+        emitter.emit('foo', count)
+        console.log("Post-emitter message for count: " + count)
+        count++
+    }
     return num
 }
