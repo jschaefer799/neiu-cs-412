4cd1b7ab4f7a47928a114521a224a04a1b792cd7
jschaefer799 <70353044+jschaefer799@users.noreply.github.com>
2020-10-14 12:40:58 -0500

    Add existing file

--- campsearch/server.js
+++ campsearch/server.js
@@ -1,31 +1,45 @@
 const http = require('http')
 const fs = require('fs')
 const path = require('path')
-
+const mime = require('mime-types')
+const routeMap ={
+    '/': './public/views/index.html',
+    '/about': './public/views/about.html'
+}
 
 let app = http.createServer((request, response) => {
     console.log('Request starting...', request.url)
 
-    let filePath = '.' + request.url
-    if (filePath === './')
-        filePath = './public/views/index.html'
+    let route = routeMap[request.url]
 
-    if (fs.existsSync(filePath)) {
-        fs.readFile(filePath, function (error, content) {
+    if (!route)
+        route = '.' + request.url
+    let contentType = mime.lookup(route)
+    //let filePath = '.' + request.url
+    //if (filePath === './')
+        //filePath = './public/views/index.html'
+
+    if (fs.existsSync(route)) {
+        fs.readFile(route, function (error, content) {
             if (error) {
                 response.writeHead(500)
                 response.end()
             } else {
+
+
+                /*
                 let contentType = ''
-                if (path.extname(filePath) === '.html')
+                if (path.extname(route) === '.html')
                     contentType = 'text/html'
-                else if (path.extname(filePath) === '.jpg') {
+                else if (path.extname(route) === '.jpg') {
                     contentType = 'image/jpeg'
-                } else if (path.extname(filePath) === '.css') {
+                } else if (path.extname(route) === '.css') {
                     contentType = 'text/css'
-                } else if (path.extname(filePath) === '.js') {
+                } else if (path.extname(route) === '.js') {
                     contentType = 'text/javascript'
                 }
+                */
+
                 response.writeHead(200, {'Content-Type': contentType})
                 response.end(content, 'utf-8')
             }
